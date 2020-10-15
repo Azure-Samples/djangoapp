a2fc34a0bd0007477d47dfb2b582d7fe110214f7
Xin Shi <xshi@microsoft.com>
2020-10-15 11:00:57 -0700

    Expand date time control by default

--- polls/admin.py
+++ polls/admin.py
@@ -9,7 +9,7 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
