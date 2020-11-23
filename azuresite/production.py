bc2ca442f9e04ee5c99dc2fc35e761cd22ba039c
Jason Freeberg <jafreebe@microsoft.com>
2020-11-23 11:10:38 -0800

    Revert temporary db changes

--- azuresite/production.py
+++ azuresite/production.py
@@ -22,23 +22,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # DBHOST is only the server name, not the full URL
-# hostname = os.environ['DBHOST']
+hostname = os.environ['DBHOST']
 
 # Configure Postgres database; the full username is username@servername,
 # which we construct using the DBHOST value.
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ['DBNAME'],
-#         'HOST': hostname + ".postgres.database.azure.com",
-#         'USER': os.environ['DBUSER'] + "@" + hostname,
-#         'PASSWORD': os.environ['DBPASS'] 
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DBNAME'],
+        'HOST': hostname + ".postgres.database.azure.com",
+        'USER': os.environ['DBUSER'] + "@" + hostname,
+        'PASSWORD': os.environ['DBPASS'] 
     }
 }
