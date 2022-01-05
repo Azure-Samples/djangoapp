8ebc8cb35414261558c110fb177df266f68e7d3f
Karen <96887635+KJSonnenberg@users.noreply.github.com>
2022-01-05 08:41:38 -0500

    First major test changes

--- polls/models.py
+++ polls/models.py
@@ -18,7 +18,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=100)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
