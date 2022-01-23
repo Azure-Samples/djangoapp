cc4f8324c9abf479647e51b473a13b5eeb1b60df
KJSonnenberg <kjsonnenberg@me.com>
2022-01-23 23:57:57 +0000

    set server settings

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'OPTIONS': {
+            'service': 'my_service',
+            'passfile': '.my_pgpass',
+        },
     }
 }
 
