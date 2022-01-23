a31d56f185897595a77a93ee6461316d7b9296ae
KJSonnenberg <kjsonnenberg@me.com>
2022-01-23 23:30:32 +0000

    changed polls config

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
-    'papermasters.apps.PapermastersConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
