a51676eda92581c35756a3230fdddac054811b55
KJSonnenberg <kjsonnenberg@me.com>
2022-01-23 23:26:23 +0000

    Reconfigured polls to papermasters

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'papermasters.apps.PapermastersConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- papermasters/views.py
+++ papermasters/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the Paper Masters index.")
 
 # Create your views here.
