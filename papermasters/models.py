c4470b29dc0a293a23ca76fbcc278af6d573ed73
KJSonnenberg <kjsonnenberg@me.com>
2022-01-24 01:00:04 +0000

    Created the Models

--- papermasters/models.py
+++ papermasters/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class subject(models.Model):
+    name = models.CharField(max_length=100)
+
+class subtopic(models.Model):
+    subtopic_text = models.CharField(max_length=200)
+    subject = models.ForeignKey(subject, on_delete=models.CASCADE)
+
+class topic(models.Model):
+    topic_text = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    content = models.TextField(blank=True)
+    related = models.TextField(blank=True)
+    image = models.ImageField(upload_to='papermasters/media/images', blank=True)
+    image_url = models.URLField(blank=True)
+    lower_content = models.TextField(blank=True)
+    subject = models.ForeignKey(subject, on_delete=models.CASCADE)
+    subtopic = models.ForeignKey(subtopic, on_delete=models.CASCADE)
