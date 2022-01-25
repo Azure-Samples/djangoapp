54a1248215697ec7478ffab56134af1344fb2603
KJSonnenberg <kjsonnenberg@me.com>
2022-01-25 19:47:49 +0000

    fix

--- papermasters/views.py
+++ papermasters/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic.detail import DetailView
-from papermasters.models import topic
 
 
 def index(request):
