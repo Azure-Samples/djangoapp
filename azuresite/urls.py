86ecc179887f2600463ec42bbe68748af55b260a
KJSonnenberg <kjsonnenberg@me.com>
2022-01-26 01:19:20 +0000

    again

--- azuresite/urls.py
+++ azuresite/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('.', include('papermasters.urls')),
     path('papermasters/', include('papermasters.urls')),
     path('admin/', admin.site.urls),
 ]
