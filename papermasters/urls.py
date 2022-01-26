0e445b0d2ce3e625e5f7bb97caab2e79bc04a7ab
KJSonnenberg <kjsonnenberg@me.com>
2022-01-26 00:55:36 +0000

    deleted index view

--- papermasters/urls.py
+++ papermasters/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('.', views.index, name='index'),
+    path('.', views.topic, name= 'topic'),
     path('<slug:slug>', views.topic, name='topic'),
 ]
