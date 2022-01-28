9acb9c5984bf167b959f6137b859d5aa33490cef
KJSonnenberg <kjsonnenberg@me.com>
2022-01-28 18:29:22 +0000

    set up for slugs

--- papermasters/urls.py
+++ papermasters/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.topic, name= 'topic'),
+    path('<int:pk>',  views.topic, name='topic'),
 
 ]
--- papermasters/views.py
+++ papermasters/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def topic(request):
-    return render(request, 'papermasters/topic.html')
+    return render(request, 'topic.html')
