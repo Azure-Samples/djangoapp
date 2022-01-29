5e016887d1574e1493bb8b99058a03db086ace28
Karen <kjsonnenberg@me.com>
2022-01-29 21:07:04 +0000

    changed path

--- toplevel/urls.py
+++ toplevel/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.toplevelView, name='toplevelView'),
+    path('', views.toplevel, name='toplevel_view'),
 ]
--- toplevel/views.py
+++ toplevel/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def toplevelView(request):
+def toplevel_view(request):
     return render(request, 'toplevel/toplevel.html')
