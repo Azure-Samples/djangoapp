b8dd6eb5250fcf27853d51b8edf13632a1dc5863
Karen <kjsonnenberg@me.com>
2022-01-31 17:43:55 +0000

    change

--- toplevel/admin.py
+++ toplevel/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Resource, Service
 
 class ResourceAdmin(admin.ModelAdmin):
-    list_display = ('title', 'meta_description', 'type_of_resource',)
+    list_display = ('title', 'type_of_resource',)
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Resource, ResourceAdmin)
