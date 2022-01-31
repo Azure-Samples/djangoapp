5c7a48ef694d88eeb595a137b16a1703912caa1b
Karen <kjsonnenberg@me.com>
2022-01-31 03:57:37 +0000

    ok

--- toplevel/urls.py
+++ toplevel/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'toplevel'
+
 urlpatterns = [
-    path('', views.toplevel_view, name='toplevel_view'),
+    path('toplevel/', views.toplevel_view, name='toplevel_view'),
     path('services/<slug:slug>/', views.services_view, name='services_view'),
     path('resources/<slug:slug>/', views.resources_view, name='resources_view'),
 ]
