e27dbea0bd34b3bf2488d07abaab76a4793e3c1c
Karen <kjsonnenberg@me.com>
2022-01-31 00:43:47 +0000

    migrating in case

--- papermasters/views.py
+++ papermasters/views.py
@@ -1,9 +1,12 @@
 from pydoc_data.topics import topics
 from django.forms import SlugField
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from .models import topic
-
+from papermasters.models import topic
 
 def topic_view(request, subject, subtopic, slug):
-    topic_object = get_object_or_404(topic, slug=slug)
-    return render(request, 'papermasters/topic.html',{'topic_object':topic})
+    try:
+        p = topic.objects.get(pk=id)
+    except topic.DoesNotExist:
+        raise Http404("Sorry, nothing here to see")
+    return render(request, 'papermasters/topic.html',{'topic':p})
