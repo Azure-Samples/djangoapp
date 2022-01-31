720c85063208f94d6d25b3cf937b511d578fe718
Karen <kjsonnenberg@me.com>
2022-01-31 02:34:12 +0000

    ok

--- papermasters/urls.py
+++ papermasters/urls.py
@@ -7,4 +7,5 @@ app_name='papermasters'
 urlpatterns = [
     path('', views.topic_view, name= 'topic_view'),
     path('<str:subject>/<str:subtopic>/<slug:slug>/', views.topic_view, name= 'topic_view')
+    
 ]
