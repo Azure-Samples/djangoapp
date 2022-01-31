c5c585b840f8ed1fd83bb398455cded9f60f102a
Karen <kjsonnenberg@me.com>
2022-01-31 02:38:09 +0000

    ugh

--- papermasters/views.py
+++ papermasters/views.py
@@ -4,9 +4,6 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import topic
 
-def topic(request):
+def topic_view(request):
     return render(request, 'papermasters/topic.html')
 
-def topic_view(request, slug):
-    t = get_object_or_404(topic,slug=slug)
-    return render(request, 'papermasters/topic.html',{'t':topic})
