65efebbb94058139a864e9ed8ec243f03698da54
Karen <kjsonnenberg@me.com>
2022-02-01 16:57:19 -0500

    9first

--- papermasters/urls.py
+++ papermasters/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name='papermasters'
 
 urlpatterns = [
-    path('home/', views.index, name='index'),
-    path('', views.topic_view, name= 'topic_view'),
-    path('papermasters/<int:id>/', views.detail_topic, name= 'detail_topic'),
+    path('', views.index, {'topdetails':''}, name='home'),
+    #path('', views.topic_view, name= 'topic_view'),
+    path('<str:topdetails>', views.index, name= 'index'),
 ]
