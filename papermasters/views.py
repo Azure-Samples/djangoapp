b5a0e7612c8d9b1d3ae9892eda5e90620303871a
Karen <kjsonnenberg@me.com>
2022-02-01 01:38:17 +0000

    pastg

--- papermasters/views.py
+++ papermasters/views.py
@@ -1,8 +1,8 @@
-from pydoc_data.topics import topics
-from django.forms import SlugField
+
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import topic
+
 
 def topic_view(request):
     return render(request, 'papermasters/topic.html')
@@ -11,8 +11,11 @@ def topic_view(request):
     topic_objects = topic.objects.all()[:5]
     return render(request, 'papermasters/topic.html', {'topic_objects':topic_objects})
 
-def detail(request, id):
-    topic_details = get_object_or_404(topic, id=self.kwargs['slug'])
-    return render(request, 'papermasters/topic-details.html',{'topic_detatils':topic_details})
+def detail_topic(request, id):
+    obj = topic.objects.get(id=id)
+    context = {
+        "object": obj
+    }
+    return render(request, 'papermasters/topic_details.html', context)
 
 
