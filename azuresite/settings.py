383d3a13c9c6b08b1830df30905c7547e62b4705
Karen <kjsonnenberg@me.com>
2022-02-01 14:16:34 -0500

    added config strucure

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -33,14 +33,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'papermasters.apps.PapermastersConfig'
+    'toplevel.apps.ToplevelConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'papermasters',
-    'toplevel',
 ]
 
 MIDDLEWARE = [
