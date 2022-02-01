b9a397c546fa487f0a66dca4cfd6f4725a71f306
Karen <kjsonnenberg@me.com>
2022-02-01 16:57:12 -0500

    cht 9 First

--- papermasters/views.py
+++ papermasters/views.py
@@ -5,21 +5,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import topic
 
 
-def topic_view(request):
-    return render(request, 'papermasters/topic.html')
-
 def index(request):
-    return render(request, 'papermasters/topic.html')
+    return render(request, 'papermasters/index.html')
 
 def topic_view(request):
     topic_objects = topic.objects.all()[:5]
     return render(request, 'papermasters/topic.html', {'topic_objects':topic_objects})
 
-def detail_topic(request, id):
-    obj = topic.objects.get(id=id)
-    context = {
-        "object": obj
-    }
-    return render(request, 'papermasters/topic_details.html', context)
+
 
 
