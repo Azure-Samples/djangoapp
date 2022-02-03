41487abf003d8751b165b671aeb792fb7120df4f
Karen <kjsonnenberg@me.com>
2022-02-03 14:23:49 -0500

    changes

--- toplevel/urls.py
+++ toplevel/urls.py
@@ -6,6 +6,6 @@ app_name = 'toplevel'
 urlpatterns = [
     path('', views.IndexView, name='IndexView'),
     path('toplevel/', views.toplevel_view, name='toplevel_view'),
-    path('services/<slug:slug>', views.services_view, name='services_view'),
-    path('resources/<slug:slug>/', views.resources_view, name='resources_view'),
+    path('<slug:slug>', views.services_view, name='services_view'),
+   
 ]
