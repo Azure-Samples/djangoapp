6ca7e2c7bfc2878026b48876c87604fb7f35650f
Karen <kjsonnenberg@me.com>
2022-02-12 10:54:01 -0500

    admin view change

--- topics/admin.py
+++ topics/admin.py
@@ -7,14 +7,14 @@ admin.site.register(subject)
 
 
 class SubtopicAdmin(admin.ModelAdmin):
-    list_display = ('subtopic_text', 'subject', 'slug',)
+    list_display = ('subtopic_text', 'subject', 'slug')
     ordering = ('subject', 'subtopic_text')
     search_fields = ('topic_text', 'content')
 
 admin.site.register(subtopic, SubtopicAdmin)
 
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('topic_text', 'subject', 'subtopic', 'update_date',)
+    list_display = ('topic_text', 'subject', 'subtopic', 'new_url', 'update_date')
     ordering = ('subject', 'subtopic', 'topic_text')
     search_fields = ('topic_text', 'content')
 
