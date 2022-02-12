09b1a2911b9f99b0e19ca38a47bbf21645cbb9d9
Karen <kjsonnenberg@me.com>
2022-02-12 18:27:50 -0500

    subject

--- topics/urls.py
+++ topics/urls.py
@@ -5,6 +5,6 @@ app_name='topics'
 
 urlpatterns = [
     path('', views.index, {'slug':''}, name='home'),
-    path('<str:subject>/<slug:slug>', views.subject_view, name= 'subject_view'),
+    path('<slug:slug>', views.subject_view, name= 'subject_view'),
     path('<str:subject>/<str:subtopic>/<slug:slug>', views.index, name= 'index'),
 ]
