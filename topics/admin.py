783fd97532013b9cb87aa9d2c55ba225b836e746
Karen <kjsonnenberg@me.com>
2022-02-12 12:08:10 -0500

    id fileds to all admin views

--- topics/admin.py
+++ topics/admin.py
@@ -2,13 +2,18 @@ from django.contrib import admin
 from .models import topic, subject, subtopic
 
 
-admin.site.register(subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('id','subject_text', 'slug')
+    ordering = ('id', 'subject_text')
+    search_fields = ('subject_text', 'content')
+
+admin.site.register(subject, SubjectAdmin)
 
 
 
 class SubtopicAdmin(admin.ModelAdmin):
-    list_display = ('subtopic_text', 'subject', 'slug')
-    ordering = ('subject', 'subtopic_text')
+    list_display = ('id','subtopic_text', 'subject', 'slug')
+    ordering = ('id','subject', 'subtopic_text')
     search_fields = ('topic_text', 'content')
 
 admin.site.register(subtopic, SubtopicAdmin)
