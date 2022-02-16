c25758f032950d8568970a0557624bc5b23620cf
Karen <kjsonnenberg@me.com>
2022-02-16 11:41:09 -0500

    added breadcrumb

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions.db.fields',
+    'dynamic_breadcrumbs',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'dynamic_breadcrumbs.context_processors.breadcrumbs',
             ],
         },
     },
