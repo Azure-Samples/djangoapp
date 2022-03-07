d617706c55bd9b643cef8d782b0a5a2973b7496d
Karen <kjsonnenberg@me.com>
2022-03-07 07:36:26 -0500

    aga

--- topics/admin.py
+++ topics/admin.py
@@ -3,7 +3,7 @@ from .models import topic
 
 class TopicAdmin(admin.ModelAdmin):
     list_display = ('id', 'topic_text', 'new_url', 'update_date')
-    ordering = ('id', 'subject', 'subtopic', 'topic_text')
+    ordering = ('id', 'topic_text')
     search_fields = ('topic_text', 'content')
 
 admin.site.register(topic, TopicAdmin)
