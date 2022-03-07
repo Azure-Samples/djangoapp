faabf802cb6c3cfe7a792fb1c5ff8ffcc2b540b2
Karen <kjsonnenberg@me.com>
2022-03-07 07:23:19 -0500

    fixed views

--- topics/views.py
+++ topics/views.py
@@ -5,20 +5,6 @@ from django.shortcuts import render
 from .models import topic
 
 
-def index(request, slug):
-    
-    slug = slug
-    pg = topic.objects.get(slug=slug)
-    context = {
-        'topic':pg.topic_text,
-        'meta_title':pg.meta_title,
-        'content':pg.content,
-        'related':pg.related,
-        'description':pg.description,
-    }
-    return render(request, 'topics/topic.html', context)
-
-
 def topic_view(request):
     topic_objects = topic.objects.all()[:5]
-    return render(request, 'topics/topic.html', {'topic_objects':topic_objects})# Create your views here.
+    return render(request, 'topics/topic.html', {'topic_objects':topic_objects})
