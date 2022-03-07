77be3460cb4f975a863b1e05174b7db8beb86348
Karen <kjsonnenberg@me.com>
2022-03-07 07:38:03 -0500

    again and aagin

--- topics/admin.py
+++ topics/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import topic
 
 class TopicAdmin(admin.ModelAdmin):
-    list_display = ('id', 'topic_text', 'new_url', 'update_date')
+    list_display = ('id', 'topic_text', 'new_url')
     ordering = ('id', 'topic_text')
     search_fields = ('topic_text', 'content')
 
