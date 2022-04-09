0461d459f1a2684c737d038a2262d0b367eb2cdd
Karen <kjsonnenberg@me.com>
2022-04-09 10:13:39 -0400

    changed

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -85,8 +85,11 @@ WSGI_APPLICATION = 'azuresite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['pm_local'],
+        'USER': os.environ['postgres'],
+        'PASSWORD': os.environ['Efromm1967'],
+        'HOST': os.environ['localhost'],
     }
 }
 
